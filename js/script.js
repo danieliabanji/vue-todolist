754f258852e8d49e2efe3f559532ee0e90431a1d
danieliabanji <danieliabanji@yahoo.com>
2022-10-28 13:52:06 +0200

    create repo

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const {createApp} = Vue;
+
+const app = createApp({
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        
+            
+    },
+    computed:{
+
+    },
+    mounted(){
+        console.log('in mounted');
+    }
+});
+app.mount('#app');
