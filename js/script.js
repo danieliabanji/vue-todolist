33b07574943b5e6b15695f9c107ae82f0a4ba163
danieliabanji <danieliabanji@yahoo.com>
2022-10-28 17:16:46 +0200

    da stilizzare

--- js/script.js
+++ js/script.js
@@ -1,20 +1,63 @@
-const {createApp} = Vue;
+// Rifare l'esercizio della to do list argomento dell to-do list a piacere :faccia_che_festeggia:.
+// Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Bonus:
+// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+// 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 
+
+
+const { createApp } = Vue;
+
+let id = 0
 const app = createApp({
-    data(){
-        return{
-            
+    data() {
+        return {
+
+            newTodo: '',
+            hasError: false,
+            todos: [{
+                id: id++,
+                text: 'Studiare',
+                done: false
+            },
+            {
+                id: id++,
+                text: 'Andare in palestra',
+                done: false
+            },
+            { 
+                id: id++, 
+                text: 'Giocare al pc', 
+                done: false 
+            }]
         }
     },
     methods: {
-        
-            
-    },
-    computed:{
+        addTodo() {
+            if(this.newTodo.length >= 5){
+                this.todos.unshift({ id: id++, text: this.newTodo, done: false });
+                this.hasError = false;
+            } else {
+                this.hasError = true
+            }
+            this.newtask = '';
+        },
+        removeTodo(todo) {
+            this.todos = this.todos.filter((t) => t !== todo)
+        },
 
-    },
-    mounted(){
-        console.log('in mounted');
+        toggle(index) {
+            this.todos[index].done = !this.todos[index].done;
+        }
+
     }
-});
-app.mount('#app');
+}).mount('#app')
